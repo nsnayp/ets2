670bbdfe88250004a265ee7ffea9b514012c20c1
ivanov.n <ivanov@etsgroup.ru>
2018-11-29 17:03:13 +0300

    Images viewer

--- components/ImgFullscreen.js
+++ components/ImgFullscreen.js
@@ -1,10 +1,11 @@
 import React, { Component, } from "react";
 import { connect } from 'react-redux';
 
-import { View , ScrollView, Dimensions,Modal,Text,TouchableNativeFeedback,PanResponder, Animated, Easing} from 'react-native';
-import ScaledImage from './ScaledImage';
+import {   Dimensions,Modal, Animated} from 'react-native';
 
 import {setVisible} from '../actions/PhotoViewer';
+
+import PhotoViewer from './PhotoViewer';
 
 
 var screenWidth = Dimensions.get('window').width;
@@ -22,94 +23,7 @@ constructor(props) {
 
 }
 
-renderScaledImage=(image,count)=>{
 
-    return(
-        <View key={image.src+'1'} style={{width:screenWidth, height:'100%', flexDirection:'column',justifyContent:'center' }}>
-            <ScaledImage width={screenWidth} uri={image.srcBig} />
-        </View>
-    )
-}
-
-getDirection = ({ moveX, moveY, dx, dy}) => {
-	const draggedDown = dy > 20;
-	const draggedUp = dy < -20;
-	const draggedLeft = dx < -20;
-	const draggedRight = dx > 20;
-
-	let dragDirection = '';
-  
-	if (draggedDown || draggedUp) {
-	  if (draggedDown) dragDirection += 'dragged down '
-	  if (draggedUp) dragDirection +=  'dragged up ';
-	}
-  
-	if (draggedLeft || draggedRight) {
-	  if (draggedLeft) dragDirection += 'dragged left '
-      if (draggedRight) dragDirection +=  'dragged right ';
-      return false;
-	}
-  	if (dragDirection) return true;
-}
-
-getDirection1=(g,i)=>{
-    console.log(g,i)
-    /*if(Math.abs(dy)>Math.abs(dx) ){
-        console.log('false',Math.abs(dy),Math.abs(dx))
-        return false
-        
-    }
-    console.log('true',Math.abs(dy),Math.abs(dx),dy,dx)
-    return true*/
-    return true
-}
-
-_panResponder = PanResponder.create({
-    onMoveShouldSetResponderCapture: (e,g) => true,
-    onMoveShouldSetPanResponderCapture: (e,g) => true,
-    onResponderTerminationRequest: (e,g) => true,
-
-    onStartShouldSetPanResponder: (e,g)=> false,
-    onMoveShouldSetPanResponder:(e, g) => this.getDirection1(g),
-	onPanResponderMove:(e, gest) => {
-        var x = Math.abs(gest.dx)
-        var y = Math.abs(gest.dy)
-
-        if(y>x){
-            Animated.event([
-                null , 
-                {dy: this.state.translateY},
-            ])(e, gest)
-        }
-	},
-
-    onPanResponderRelease: (e, gest) => {
-        //console.log(gest)
-        Animated.timing(this.state.translateY, {
-          toValue: 0 ,
-		  duration: 100,
-		
-        }).start();
-    }
-});
-
-componentWillReceiveProps=()=>{
-    
-}
-
-componentDidMount=()=>{
-    
-}
-
-componentDidUpdate=()=>{
-    if(this.scrollView1){
-        var inst = this
-        setTimeout(()=>{
-            inst.scrollView1.scrollTo({x: screenWidth*inst.props.activeImage, y: 0, animated: false});
-        },100)
-        
-    }
-}
 
 render=()=>{ 
 
@@ -119,18 +33,10 @@ render=()=>{
             onRequestClose={ ()=>{ this.props.setVisible(false)  } }
             transparent={true}
             visible={this.props.visible}
+            
             >
- 
-            <ScrollView
-                onScroll= {function(event) {
-                    console.log(event.nativeEvent.contentOffset.x);
-                }}
-                pagingEnabled ref={el => { this.scrollView1 = el }}  
-                horizontal  
-                style={{ width:'100%', height:'100%', backgroundColor:'#000', flexDirection:'row'}}  >
-                    {Object.values(this.props.images).map( (image,index) => this.renderScaledImage(image,index))}
-            </ScrollView>
-                
+             <PhotoViewer  images={this.props.images} activeImage={this.props.activeImage}></PhotoViewer>
+
         </Modal>
     )
 }
---
+++ components/PhotoViewer.js
@@ -0,0 +1,197 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View,PanResponder,Animated,Dimensions,Image,Easing  } from 'react-native';
+import ScaledImage from './ScaledImage';
+
+
+const w = Dimensions.get("window").width;
+
+export default class PhotoViewer extends Component {
+
+
+state = {
+  scaled:false,
+  scale: new Animated.Value(1),
+  left: new Animated.Value(-w),
+  index:1,
+  rtht:false,
+  images:[]
+};
+
+
+
+
+
+componentWillMount() {
+
+    this.setState({
+        images: [...[''],...this.props.images],
+        index:this.props.activeImage+1
+    })
+
+
+  this._panResponder = PanResponder.create({
+    onMoveShouldSetResponderCapture: () => true,
+    onMoveShouldSetPanResponderCapture: () => true,
+    onStartShouldSetPanResponderCapture:()=>true,
+
+    onPanResponderMove: (e,g)=>{
+      //console.log(e.nativeEvent)
+      const touches = e.nativeEvent.touches;
+
+
+      if(touches.length==1){
+        if(this.state.scaled){
+         // console.log('смещение')
+          //const a={dx:(g.dx-w)}
+          //Animated.event([null, {dx: this.state.left}])(e,a)
+        }else{
+          const a={dx:(g.dx-w)}
+          Animated.event([null, {dx: this.state.left}])(e,a)
+        }
+        
+      }else if(touches.length==2){
+        const a={scale : 2}
+        Animated.event([null, {scale: this.state.scale}])(e,a);
+        this.setState({scaled:true})
+      }
+      
+
+    },
+    onPanResponderRelease: (e, g) => {
+    
+      if(!this.state.scaled){
+       // this.setState({rtht:true})
+
+      
+    //Предыдущее фото
+      if(g.dx< (-w/2) || g.vx<-0.1){
+        if(this.state.index<this.state.images.length-1){
+
+          
+          Animated.timing(this.state.left, {
+            toValue: - w-w,
+            duration: 115,
+            easing: Easing.out(Easing.poly(2)),
+          }).start(()=>{
+            var inst = this
+            setTimeout(()=>{
+              inst.setState({index:inst.state.index+1},()=>{
+                g.dx=-w
+                Animated.event([null, {dx: inst.state.left}])(e,g)
+              })
+            },100)
+           
+            
+          });
+
+
+
+
+        }else{
+          Animated.timing(this.state.left, {
+            toValue: -w,
+            duration: 115
+          }).start()
+        }
+
+      }else if(g.dx> (w/2) || g.vx>0.1 ){
+
+        if(this.state.index>1){
+
+          
+
+          Animated.timing(this.state.left, {
+            toValue:  0,
+            duration: 115,
+            easing: Easing.out(Easing.poly(2))
+          }).start(()=>{
+            var inst = this
+            setTimeout(()=>{
+              inst.setState({index:inst.state.index-1},()=>{
+                g.dx=-w
+                Animated.event([null, {dx: inst.state.left}])(e,g)
+              })
+            },100)
+            
+            
+          });
+
+        }else{
+          Animated.timing(this.state.left, {
+            toValue: -w,
+            duration: 115
+          }).start()
+        }
+      }
+      else{
+        Animated.timing(this.state.left, {
+          toValue:  -w,
+          duration: 115,
+          easing: Easing.easeIn
+        }).start()
+      }
+
+    }
+
+    }
+  })
+
+  
+
+}
+
+
+renderImage=(item,index)=>{
+  if(index!=0){
+    return(
+      <Animated.View style={{backgroundColor:'transparent', width:'100%', height:'100%', justifyContent:'center', alignItems:'center', transform:[{scale:this.state.scale}] }}>
+        <ScaledImage width={w} uri={item.srcBig} />
+      </Animated.View>
+    )
+  }else{
+    return null;
+  }
+}
+
+
+renderItem =(item,index)=>{
+  
+  if(index == this.state.index || index == this.state.index-1 || index == this.state.index+1){
+      return(
+        <View key={index} style={{backgroundColor:'transparent', width:'100%', height:'100%', justifyContent:'center', alignItems:'center',padding:2,overflow:'hidden' }}>
+         {this.renderImage(item,index)}
+        </View>
+      )
+
+  }else{
+    return null
+  }
+}
+
+
+
+
+  render() {
+
+
+    return (
+        <View
+        style={{flex:1, flexDirection:'row',backgroundColor:'#000'}}
+        {...this._panResponder.panHandlers}
+        >
+          <Animated.View  style={{flex:1,width:'100%', height:'100%' ,   flexDirection:'row', position:'absolute',left:this.state.left}}>
+            {this.state.images.map((item, index)=>{
+              return this.renderItem(item,index)
+            })}
+          </Animated.View>
+          <View style={{position:'absolute', left:0, top:0, padding:16}}>
+            <Text style={{color:'#fff'}}>Фото {this.state.index} из {this.state.images.length-1}</Text>
+          </View>
+        </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  
+});
